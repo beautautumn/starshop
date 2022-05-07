51b28be3ca7c0db717c3a9a4c9621490bda3a572
samson <shu.sh@163.com>
2022-05-08 00:59:36 +0800

    adjust project structure to module based structure.

---
+++ foundation/chain/src/test/java/com/stardata/starshop2/chaincontext/command/StarshopChainCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardata.starshop2.chaincontext.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshopChainCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
