868cad4aed7648e1682170ffb0e7c9bc9c9acc3e
samson <shu.sh@163.com>
2022-05-10 17:26:11 +0800

    Complete technical decision for tactical design.

---
+++ core/src/main/java/com/stardata/starshop2/sharedcontext/util/InitializedEntityManagerException.java
@@ -0,0 +1,10 @@
+package com.stardata.starshop2.sharedcontext.util;
+
+/**
+ * @author Samson Shu
+ * @version 1.0
+ * @email shush@stardata.top
+ * @date 2022/5/9 21:26
+ */
+public class InitializedEntityManagerException extends Exception {
+}
