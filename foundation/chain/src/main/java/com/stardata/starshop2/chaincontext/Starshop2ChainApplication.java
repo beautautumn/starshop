90080dd256ee5654f5e4e2e7ca58d670ec86d308
samson <shu.sh@163.com>
2024-01-30 16:45:25 +0800

    fix maven package can't find class error: add <classifier>exec</classifier> to pom.xml for core/api/orderCommand

---
+++ foundation/chain/src/main/java/com/stardata/starshop2/chaincontext/Starshop2ChainApplication.java
@@ -0,0 +1,24 @@
+package com.stardata.starshop2.chaincontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Samson Shu
+ * @version 1.0
+ * @email shush@stardata.top
+ * @date 2022/5/11 10:43
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.stardata.starshop2"})
+@EnableDiscoveryClient
+//@EnableDubbo
+public class Starshop2ChainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Starshop2ChainApplication.class, args);
+    }
+}
+
+
